13038afea1943bca12f05cb03e37d2e8f8ef6872
Niccolo Delfino <delfino.nicco@libero.it>
2021-03-22 16:31:00 +0100

    wip seeds

---
+++ database/seeds/BooksTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use APP\Book;
+
+class BooksTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
